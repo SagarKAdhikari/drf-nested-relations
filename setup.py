39d5790b29b2191645c197d52b892d28a7641424
SagarKAdhikari <070bct533@ioe.edu.np>
2019-04-05 13:23:07 +0545

    Added classifiers in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
@@ -12,23 +12,23 @@ setup(
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
-    license='BSD License',  # example license
-    description='A library for crud of nested relations',
+    license='BSD License',  
+    description='An extension of django rest framework that allows writing nested relations',
     long_description=README,
-    # url='https://www.example.com/',
-    # author='Your Name',
-    # author_email='yourname@example.com',
-    # classifiers=[
-    #     'Environment :: Web Environment',
-    #     'Framework :: Django',
-    #     'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
-    #     'Intended Audience :: Developers',
-    #     'License :: OSI Approved :: BSD License',  # example license
-    #     'Operating System :: OS Independent',
-    #     'Programming Language :: Python',
-    #     'Programming Language :: Python :: 3.5',
-    #     'Programming Language :: Python :: 3.6',
-    #     'Topic :: Internet :: WWW/HTTP',
-    #     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    # ],
+    url='https://github.com/SagarKAdhikari/drf-nested-relations',
+    author='Sagar Adhikari',
+    author_email='ska80117@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django Rest Framework',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
